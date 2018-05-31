f1ec5315489807ef7d19a7c970c751112e9f0559
Fabien MARTY <fabien.marty@gmail.com>
2018-05-31 15:56:34 +0200

    add copyright notice

--- telegraf_unixsocket_client/line_protocol.py
+++ telegraf_unixsocket_client/line_protocol.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 
-# NOTE:
 # This code is borrowed to the
 # https://github.com/influxdata/influxdb-python project
 # (MIT license)
 # last commit of the file: 6b5db784dcb37495c1f24e3722973878eb3dd53d
+
+# The MIT License (MIT)
+
+# Copyright (c) 2013 InfluxDB
+
+# Permission is hereby granted, free of charge,
+# to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of
+# the Software, and to permit persons to whom
+# the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+
 # pylint: disable=E1120
 
 """Define the line_protocol handler."""
